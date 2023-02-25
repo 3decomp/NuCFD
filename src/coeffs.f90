5205869b5b8b80399bc9fb58be9c126d53ad34e9
Paul <ptb0890@gmail.com>
2023-02-25 17:22:41 +0000

    Add header comment to coeffs module

--- src/coeffs.f90
+++ src/coeffs.f90
@@ -1,9 +1,23 @@
+!!!! src/coeffs.f90
+!!!
+!!!! Description
+!!!
+!!! Defines the coefficients for compact finite difference schemes on non-uniform grids.
+!!!
+!!! Provides the nucfd_coeffs module.
+!!!
+!!!! LICENSE
+!!!
+!!! SPDX-License-Identifier: BSD-3-Clause
+!!!
+
 module nucfd_coeffs
 
   implicit none
 
   private
 
-  real, parameter, public :: alpha = 1.0 / 3.0
+  real, parameter, public :: alpha = 1.0 / 3.0 ! Off-diagonal coefficient for first-derivative
+                                               ! system.
   
 end module nucfd_coeffs
