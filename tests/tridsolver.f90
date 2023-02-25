d62ce54e4bba3dfd2d2838134e0b26524b119d4a
Paul <ptb0890@gmail.com>
2023-02-25 16:29:12 +0000

    Tidy up the test suite

--- tests/tridsolver.f90
+++ tests/tridsolver.f90
@@ -14,16 +14,51 @@ module nucfd_tests
   implicit none
 
   private
+  public :: initialise_suite, finalise_suite
   public :: test_report
 
+  character(len=:), allocatable :: suite_name
+  logical, save :: passing
+  
 contains
 
+  subroutine initialise_suite(test_suite_name)
+
+    character(len=*), intent(in) :: test_suite_name
+
+    suite_name = test_suite_name
+    
+    print *, "************************************************************************"
+    print *, " "//suite_name//" test suite. "
+    print *, "------------------------------------------------------------------------"
+
+    passing = .true.
+    
+  end subroutine initialise_suite
+
+  subroutine finalise_suite()
+  
+    print *, "------------------------------------------------------------------------"
+    call test_report(suite_name)
+    print *, "************************************************************************"
+
+  end subroutine finalise_suite
+  
   subroutine test_report(test_name, test_status)
 
     character(len=*), intent(in) :: test_name
-    logical, intent(in) :: test_status
+    logical, intent(in), optional :: test_status
 
-    if (test_status) then
+    logical :: report_status
+
+    if (present(test_status)) then
+       report_status = test_status
+       passing = test_status .and. passing
+    else
+       report_status = passing
+    end if
+    
+    if (report_status) then
        print *, " "//test_name//": PASS"
     else
        print *, " "//test_name//": PASS"
@@ -38,17 +73,20 @@ program test_tridsolver
   use nucfd_tests
   
   implicit none
-  
-  logical :: passing
-  
-  print *, "************************************************************************"
-  print *, " Tridiagonal solver test suite. "
-  print *, "************************************************************************"
 
-  passing = .true.
-  
-  print *, "------------------------------------------------------------------------"
-  call test_report("Test suite", passing)
-  print *, "************************************************************************"
+  call initialise_suite("Tridiagonal solver")
+  call finalise_suite()
+
+contains
+
+  logical function solve_11_system(n)
+
+    integer, intent(in) :: n
+
+    solve_11_system = .true.
+
+    call test_report("Solve 11 system", solve_11_system)
+    
+  end function solve_11_system
   
 end program test_tridsolver
