5cfd84d584b57929d1d6dc1c76b7c74aa5b8b2d9
Paul <ptb0890@gmail.com>
2023-02-26 14:48:33 +0000

    Update comments to work better with FORD

--- examples/compute-derivatives.f90
+++ examples/compute-derivatives.f90
@@ -1,16 +1,8 @@
-!!!! examples/compute-derivatives.f90
-!!!
-!!!! Description
-!!!
-!!! A program that uses non-uniform compact finite difference schemes to compute derivatives on
-!!! (non-)uniform meshes.
-!!!
-!!!! LICENSE
-!!!
-!!! SPDX-License-Identifier: BSD-3-Clause
-!!
-
 program compute_derivatives
+  !! A program that uses non-uniform compact finite difference schemes to compute derivatives on
+  !! (non-)uniform meshes.
+  !!
+  !! SPDX-License-Identifier: BSD-3-Clause
 
   implicit none
   
