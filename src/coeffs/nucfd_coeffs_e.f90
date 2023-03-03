1832cb5b1f36dc0e5ac2f0596a7d01a6788d9bc0
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2023-03-03 18:34:12 +0000

    Document the coeff_e module

--- src/coeffs/nucfd_coeffs_e.f90
+++ src/coeffs/nucfd_coeffs_e.f90
@@ -1,4 +1,8 @@
 submodule (nucfd_coeffs) nucfd_coeffs_e
+  !! Submodule defining the coefficient acting on f_{i} for compact finite difference schemes on
+  !! non-uniform grids.
+  !!
+  !! SPDX-License-Identifier: BSD-3-Clause
 
   implicit none
 
@@ -11,6 +15,20 @@ contains
                                                 !! difference.
 
     real :: a, b, c, d ! The neighbouring coefficients
+
+#ifndef NDEBUG
+    if (size(x%stencil) /= 5) then
+       print *, "Error@coeff_e: expecting width 5 stencil, received width = ", &
+            size(x%stencil)
+       error stop
+    end if
+    if (1 - lbound(x%stencil, 1) /= 3) then
+       print *, "Error@coeff_e: expecting centre 3 stencil, received centre = ", &
+            1 - lbound(x%stencil, 1)
+       print *, size(x%stencil), lbound(x%stencil), ubound(x%stencil)
+       error stop
+    end if
+#endif
 
     a = coeff_a(x)
     b = coeff_b(x)
