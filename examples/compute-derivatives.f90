e529de5ed391e96663261b8bee23bc07071c001f
Paul <ptb0890@gmail.com>
2023-03-04 20:22:51 +0000

    Add header to compute derivatives example

--- examples/compute-derivatives.f90
+++ examples/compute-derivatives.f90
@@ -1,8 +1,12 @@
+! examples/compute-derivatives.f90
+!
+!! Example program to compute derivatives.
+!
+! SPDX-License-Identifier: BSD-3-Clause
+
 program compute_derivatives
   !! A program that uses non-uniform compact finite difference schemes to compute derivatives on
   !! (non-)uniform meshes.
-  !!
-  !! SPDX-License-Identifier: BSD-3-Clause
 
   implicit none
   
